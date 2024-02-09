50ed93e5493b4c1f4fa5b263551cf558e2322b5e
Drujehong <drujehong@gmail.com>
2024-02-09 22:05:11 +0800

    Added Error Messages to invalid city names
    Next up should be changing Dates according to Time Zone :)

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ async function fetchWeatherData(){
   try{
     const apiKeyResponse = await fetch('api_key.txt');
     const apiKey = await apiKeyResponse.text();
-    console.log(apiKey);
 
     if(apiKey.length == 0){
       throw new Error("Invalid API Key")
@@ -10,13 +9,18 @@ async function fetchWeatherData(){
     const inputCityName = document.getElementById("inputCityName").value.toLowerCase();
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputCityName}&appid=${apiKey}`);
     if(!response.ok){
+        document.querySelector(".weatherData").style.display = "none";
+        document.querySelector(".errorMessage").style.display = "block";
         throw new Error("Could not fetch resource");
     }
     const weatherData = await response.json();
     console.log(weatherData);
 
+    document.querySelector(".weatherData").style.display = "block";
+    document.querySelector(".errorMessage").style.display = "none";
+
     document.querySelector(".cityName").innerHTML = weatherData.name;
-    document.querySelector(".temp").innerHTML = (weatherData.main.temp/10).toFixed(1) + "°C";
+    document.querySelector(".temp").innerHTML = (weatherData.main.temp + (-273.15)).toFixed() + "°C";
     document.querySelector(".humidity").innerHTML = weatherData.main.humidity + "%";
     document.querySelector(".wind").innerHTML = weatherData.wind.speed + "km/h";
  
@@ -34,10 +38,16 @@ async function fetchWeatherData(){
     const localTimeInSecondsOfCity = currUTCinSeconds + shiftInSecondsUTC;
     
     var hourOfTheDay = Math.floor(localTimeInSecondsOfCity / 3600);
-    console.log("before calculation " + hourOfTheDay);
     if(hourOfTheDay < 0){
       hourOfTheDay += 12;
     }
+
+    if(hourOfTheDay >= 24)
+    {
+      hourOfTheDay = hourOfTheDay - 24;
+    }
+
+    // Fixing Dates System
 
     var minuteOfTheDay = (localTimeInSecondsOfCity / 3600) - Math.floor(localTimeInSecondsOfCity / 3600);
     minuteOfTheDay = Math.floor(minuteOfTheDay * 60);
@@ -47,6 +57,28 @@ async function fetchWeatherData(){
     
     document.querySelector(".currTime").innerHTML = formattedTime;
   
+    // Changing icons
+    const weatherIcon = document.querySelector(".weatherIcon");
+
+    if(weatherData.weather[0].main == "Clouds"){
+      weatherIcon.src = "icon-images/clouds.png";
+    }
+    else if(weatherData.weather[0].main == "Clear"){
+      weatherIcon.src = "icon-images/clear.png";
+    }
+    else if(weatherData.weather[0].main == "Drizzle"){
+      weatherIcon.src = "icon-images/drizzle.png";
+    }
+    else if(weatherData.weather[0].main == "Mist"){
+      weatherIcon.src = "icon-images/mist.png";
+    }
+    else if(weatherData.weather[0].main == "Rain"){
+      weatherIcon.src = "icon-images/rain.png";
+    }
+    else if(weatherData.weather[0].main == "Snow"){
+      weatherIcon.src = "icon-images/snow.png";
+    }
+
   }
   catch(error){
     console.error(error);
